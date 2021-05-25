52171cd2a07145b560327bd28305e45ebb843a94
Artyom Tugaryov <artyom.tugaryov@gmail.com>
2021-05-25 17:35:02 +0300

    Setup accuracy

---
+++ accuracy.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+
+source ~/intel/openvino_2021/bin/setupvars.sh
+source .venv/bin/activate
+
+root_models_path=models
+models=$(ls models -I "public")
+configs_folder=configs
+
+set -e
+
+measure_accuracy_for_model () {
+  model_path=$1
+  model_exists=`ls -1 ${model_path}/*.xml 2>/dev/null | wc -l`
+  if [ ${model_exists} == 0 ];  then
+    echo " Folder ${model_path} does not contain model. Skip"
+    return
+  fi
+  model_name=$(basename -- $(dirname -- "$model_path"))
+  accuracy_check -c ${configs_folder}/${model_name}.yml -m ${model_path} -s data/ --csv_result ${model_path}/accuracy_result.csv --async_mode 1
+}
+
+measure_accuracy_for_all_models(){
+  root_model_path=$1
+  sub_folders=$(ls ${root_model_path})
+
+  for sub_folder in ${sub_folders}
+  do
+    measure_accuracy_for_model ${root_model_path}/${sub_folder}
+  done
+
+}
+
+for model in ${models}
+do
+  measure_accuracy_for_all_models ${root_models_path}/${model}
+done
